fd238c526b1cbaf8892a8226a96d9157b57d5d75
MERT ACAR <95058500+mertAcar-TR@users.noreply.github.com>
2023-01-24 03:38:58 +0300

    24/01/2023 03:38

---
+++ MiddlewareExample.Web/Middlewares/WhiteIPAddressControlMiddleware.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Net;
+
+namespace MiddlewareExample.Web.Middlewares
+{
+	public class WhiteIPAddressControlMiddleware
+	{
+		private readonly RequestDelegate _requestDelegate;
+        private const string WhiteIpAddress = "192.188.2.1";
+
+        public WhiteIPAddressControlMiddleware(RequestDelegate requestDelegate)
+        {
+            _requestDelegate = requestDelegate;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            var reqIpAddress = context.Connection.RemoteIpAddress;
+
+            bool anyWhiteIpAddress = IPAddress.Parse(WhiteIpAddress).Equals(reqIpAddress);
+
+            if (anyWhiteIpAddress==true)
+            {
+                await _requestDelegate(context);
+            }
+            else
+            {
+                context.Response.StatusCode = HttpStatusCode.Forbidden.GetHashCode();
+                await context.Response.WriteAsync("Forbidden");
+            }
+        }
+    }
+}
+
